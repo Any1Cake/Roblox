b73002324fb453cbef92e968808041fadf7f8eb7
Any1Cake <any1cake@gmail.com>
2024-08-02 05:36:40 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,20 +1,32 @@
+--loadstring(game:HttpGet(https://raw.githubusercontent.com/Any1Cake/Roblox/main/Loader.lua, true))()
 local repo = "https://raw.githubusercontent.com/Any1Cake/Roblox/main/Scripts/Games/"
+
 local function LoadScript(ScriptName)
+    local CoreGui = game:GetService("StarterGui")
+    
     pcall(function()
         local t = 0
         repeat
             local s, r = pcall(function()
-                loadstring(game:HttpGet(repo .. ScriptName))()
+                loadstring(game:HttpGet(repo .. ScriptName, true))()
             end)
-            if not s then
+            if not success then
                 spawn(function()
-                    error(error)
+                    error(r)
                 end)
             end
             t = t + 1
             wait(1)
         until getgenv().Executed or t >= 1
     end)
+    
+    -- Notification for script loaded
+    CoreGui:SetCore("SendNotification", {
+        Title = "Script Loaded",
+        Text = "The script has been successfully loaded.",
+        Icon = "rbxassetid://10885644072",
+        Duration = 2.5,
+    })
 end
 
 local Id = game.PlaceId
@@ -26,9 +38,32 @@ local PlaceIds = {
     -- Add more games here in the format: ["GameName"] = {GameId, "ScriptName.lua"},
 }
 
+local isGameSupported = false
+local supportedGameName = ""
 for gameName, gameInfo in pairs(PlaceIds) do
     if table.find(gameInfo, GameId) then
-        LoadScript(gameInfo[2])
+        isGameSupported = true
+        supportedGameName = gameName
         break
     end
+end
+
+if isGameSupported then
+    -- Notification for supported game and loading
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "Game Supported",
+        Text = "Loading " .. supportedGameName .. " script",
+        Icon = "rbxassetid://10885644072",
+        Duration = 2.5,
+    })
+    
+    LoadScript(PlaceIds[supportedGameName][2])
+else
+    -- Notification for unsupported game
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "Game Not Supported",
+        Text = "This game is not supported by the script.",
+        Icon = "rbxassetid://10885644072",
+        Duration = 2.5,
+    })
 end
