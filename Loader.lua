bb102961fc20d22af4feba10f216a0423943127a
Mohy <any1cake@gmail.com>
2024-08-02 21:48:05 +0700

    Updated Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,24 +1,44 @@
+local SaveExecution = true -- Set the flag to true in BoolValue
+
+local replicatedStorage = game:GetService("ReplicatedStorage")
+local isGameExecutedValue = replicatedStorage:FindFirstChild("IsGameExecuted")
+if not isGameExecutedValue then
+    isGameExecutedValue = Instance.new("BoolValue")
+    isGameExecutedValue.Name = "IsGameExecuted"
+    isGameExecutedValue.Parent = replicatedStorage
+end
+
 local games = {
     [258258996] = "Miner's Haven",
     [3398014311] = "Restaurant Tycoon 2",
 }
 
 local isGameFound = false
+local isGameExecuted = isGameExecutedValue.Value -- Retrieve the flag from BoolValue
 local CoreGui = game:GetService("StarterGui")
 for placeId, gameName in pairs(games) do
     if game.PlaceId == placeId then
         isGameFound = true
-        local formattedGameName = gameName:gsub("%s+", "-") -- Replace spaces with hyphens
-        local scriptURL = "https://raw.githubusercontent.com/Any1Cake/Roblox/main/Scripts/Games/" .. formattedGameName .. ".lua"
-        
-        CoreGui:SetCore("SendNotification", {
-            Title = "Game Found!",
-            Text = "Executing " .. gameName .. " Script",
-            Icon = "rbxassetid://10885644072",
-            Duration = 1.5,
-        })
-        loadstring(game:HttpGet(scriptURL, true))()
-        break
+        if not isGameExecuted then -- Check if the game script has not been executed yet
+            isGameExecutedValue.Value = SaveExecution
+            local formattedGameName = gameName:gsub("%s+", "-") -- Replace spaces with your choice
+            local scriptURL = "https://raw.githubusercontent.com/Any1Cake/Roblox/main/Scripts/Games/" .. formattedGameName .. ".lua"
+            
+            CoreGui:SetCore("SendNotification", {
+                Title = "Game Found!",
+                Text = "Executing " .. gameName .. " Script",
+                Icon = "rbxassetid://10885644072",
+                Duration = 1.5,
+            })
+            loadstring(game:HttpGet(scriptURL, true))()
+        else
+            CoreGui:SetCore("SendNotification", {
+                Title = "Game Already Executed!",
+                Text = "The game script has already been executed.",
+                Icon = "rbxassetid://10885644072",
+                Duration = 1.5,
+            })
+        end
     end
 end
 
