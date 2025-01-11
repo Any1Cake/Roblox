b5764ca2c8d2bad163fd96b166a7a85059a4383d
Any1Cake <any1cake@gmail.com>
2025-01-11 13:34:41 +0700

    Create Loader2.lua

---
+++ Loader2.lua
@@ -0,0 +1,57 @@
+
+local SaveExecution = true 
+local replicatedStorage = game:GetService("ReplicatedStorage")
+local isGameExecutedValue = replicatedStorage:FindFirstChild("IsGameExecuted")
+if not isGameExecutedValue then
+    isGameExecutedValue = Instance.new("BoolValue")
+    isGameExecutedValue.Name = "IsGameExecuted"
+    isGameExecutedValue.Parent = replicatedStorage
+end
+
+local games = {
+    [258258996] = "Miner's Haven",
+    [3398014311] = "Restaurant Tycoon 2",
+    [9772878203] = "Raise a Floppa 2",
+    [7345818711] = "Trucking Empire",
+    [11911775162] = "Trucking Empire" -- Additional ID for Trucking Empire
+}
+
+local isGameFound = false
+local isGameExecuted = isGameExecutedValue.Value
+local CoreGui = game:GetService("StarterGui")
+for placeId, gameName in pairs(games) do
+    if game.PlaceId == placeId then
+        isGameFound = true
+        if not isGameExecuted then
+            isGameExecutedValue.Value = SaveExecution
+            local formattedGameName = gameName:gsub("%s+", "-") -- Replace spaces with your choice
+            local scriptURL = "https://raw.githubusercontent.com/Any1Cake/Roblox/main/Scripts/Games/" .. formattedGameName .. ".lua"
+            
+            CoreGui:SetCore("SendNotification", {
+                Title = "Game Found!",
+                Text = "Executing " .. gameName .. " Script",
+                Icon = "rbxassetid://10885644072",
+                Duration = 2.5,
+            })
+            
+            loadstring(game:HttpGet(scriptURL, true))()
+
+        else
+            CoreGui:SetCore("SendNotification", {
+                Title = "Game Already Executed!",
+                Text = "The game script has already been executed.",
+                Icon = "rbxassetid://10885644072",
+                Duration = 2.5,
+            })
+        end
+    end
+end
+
+if not isGameFound then
+    CoreGui:SetCore("SendNotification", {
+        Title = "Game Not Found!",
+        Text = "The game you are playing is not in the list.",
+        Icon = "rbxassetid://10885644072",
+        Duration = 2.5,
+    })
+end
